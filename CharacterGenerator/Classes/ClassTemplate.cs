350f188227c4ff2ddf2a266b39069a23b6659e0e
John B <godlycomplex@gmail.com>
2018-07-23 20:56:47 -0400

    Initial commit.

---
+++ CharacterGenerator/Classes/ClassTemplate.cs
@@ -0,0 +1,14 @@
+﻿namespace CharacterGenerator.Classes
+{
+    class ClassTemplate
+    {
+        int[] BaseAttackBonus { get; set; }
+        int[] ReflexSave { get; set; }
+        int[] FortificationSave { get; set; }
+        int[] WillSave { get; set; }
+        int HitDice { get; set; }
+        int SkillPoints { get; set; }
+        bool[] AbilityStatPoint { get; set; }
+        int[] Feats { get; set; }
+    }
+}
---
+++ CharacterGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CharacterGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fighter = new CharacterClasses();
+            var cleric = new CharacterClasses();
+            var rogue = new CharacterClasses();
+        }
+    }
+    class Character
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int BaseAttack { get; set; }
+        public int ArmorClass { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constition { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+    }
+
+    class CharacterLevelups
+    {
+
+    }
+
+    public class CharacterClasses
+    {
+
+        List<CharacterLevelups> Levels;
+
+    }
+}
