f3b75a05e8c91d1fe3f34fac089fd208dbc8bda2
John B <godlycomplex@gmail.com>
2018-07-26 23:48:31 -0400

    Added cleric bab chart

---
+++ CharacterGenerator/Classes/Cleric.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterGenerator.Classes
+{
+    public class Cleric : ClassTemplate
+    {
+        private void cleric()
+        {
+            this.baseAttackBonus = new int[][]
+            {
+                new int[] { 0},
+                new int[] { 1},
+                new int[] { 2},
+                new int[] { 3},
+                new int[] { 3},
+                new int[] { 4},
+                new int[] { 5},
+                new int[] { 6, 1 },
+                new int[] { 6, 1 },
+                new int[] { 7, 2 },
+                new int[] { 8, 3 },
+                new int[] { 9, 4 },
+                new int[] { 9, 4 },
+                new int[] { 10, 5 },
+                new int[] { 11, 6, 1 },
+                new int[] { 12, 7, 2 },
+                new int[] { 12, 7, 2 },
+                new int[] { 13, 8, 3 },
+                new int[] { 14, 9, 4 },
+                new int[] { 15, 10, 5 },
+            };
+        }
+    }
+}
